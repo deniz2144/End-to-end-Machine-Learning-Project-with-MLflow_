b9e3ecd856f54eea15af0a81db8532340f82b60f
Deniz Ünlü <dunlu4822@gmail.com>
2025-06-25 16:41:33 +0300

    first commit

--- src/mlProject/pipeline/stage_04_mode_trainer.py
+++ src/mlProject/pipeline/stage_04_mode_trainer.py
@@ -1,19 +1,20 @@
 from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.model_trainer import ModelTrainer
 from mlProject import logger
-from mlProject.components.data_validation import DataValidation
 
-STAGE_NAME = "Data Validation stage"
 
-class DataValidationTrainingPipeline:
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_validation_config = config.get_data_validation_config()
-        data_validation = DataValidation(config=data_validation_config)
-        data_validation.validate_all_columns()
-
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
 
 
 
@@ -21,10 +22,9 @@ class DataValidationTrainingPipeline:
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataValidationTrainingPipeline()
+        obj = ModelTrainerTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
-
